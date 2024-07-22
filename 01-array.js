0113fb447301342805a0723b02b202e4f83fd944
dionisioR <72356766+dionisioR@users.noreply.github.com>
2024-07-22 17:08:01 -0300

    Update 2024-07-22 17:07
    Update 2024-07-22 17:07

---
+++ 01-array.js
@@ -0,0 +1,11 @@
+const notas = [10, 6.5, 8, 7.5]
+
+let media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4
+console.log(media)
+
+media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+console.log(media)
+
+console.log('----------------------')
+const lista = [10, 'oi', true, [1, 2], {a:'B'}]
+console.log(lista)
---
+++ 03-excluindoElementos.js
@@ -0,0 +1,5 @@
+const notas = [10, 6, 8, 5.5, 10]
+console.log(notas)
+
+notas.pop() // remove o último elemento do array
+console.log(notas)
